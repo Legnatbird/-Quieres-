70c69add4adf91b4a03026da0cbd6266a072b52a
Legnatbird <50637445+WholeMuffin@users.noreply.github.com>
2021-11-16 11:31:05 -0500

    Se agrega carousel
    uwu

--- scripts.js
+++
@@ -1,7 +0,0 @@
-no.addEventListener("mousemove",()=>{
-  const x = Math.round(Math.random()*80);
-  const y = Math.round(Math.random()*80);
-  no.style.left = x+"%";
-  no.style.top = y+"%";
-  no.style.setProperty("transform",`translate(-${x}%,-${y}%)`);
-});
