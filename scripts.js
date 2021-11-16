0f0dc105c7d737c928b558e38606e67d5d4e6aaf
Legnatbird <50637445+WholeMuffin@users.noreply.github.com>
2021-11-16 09:31:03 -0500

    Prueba para celulares

--- scripts.js
+++ scripts.js
@@ -5,3 +5,10 @@ no.addEventListener("mousemove",()=>{
   no.style.top = y+"%";
   no.style.setProperty("transform",`translate(-${x}%,-${y}%)`);
 });
+no.addEventListener("touchstart",()=>{
+    const x = Math.round(Math.random()*80);
+    const y = Math.round(Math.random()*80);
+    no.style.left = x+"%";
+    no.style.top = y+"%";
+    no.style.setProperty("transform",`translate(-${x}%,-${y}%)`);
+  });
